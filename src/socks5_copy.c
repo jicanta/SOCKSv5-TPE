89b35e24ba77c49e02a5b849efa6944847397f72
ldicandia <ldicandia@itba.edu.ar>
2025-12-08 15:41:45 -0300

    Fix: Correccion Selector Pre-Entrega

--- src/socks5_copy.c
+++ src/socks5_copy.c
@@ -37,7 +37,7 @@ static struct copy_st* get_connection_state(struct selector_key* key) {
   }
 }
 
-static unsigned handle_read_eof(struct copy_st* conn) {
+static unsigned handle_read_eof(struct copy_st* conn, fd_selector s) {
   shutdown(*conn->fd, SHUT_RD);
   conn->duplex &= ~OP_READ;
 
@@ -46,19 +46,51 @@ static unsigned handle_read_eof(struct copy_st* conn) {
     conn->other->duplex &= ~OP_WRITE;
   }
 
+  if (conn->duplex == OP_NOOP) {
+      if (*conn->fd != -1) {
+        selector_unregister_fd(s, *conn->fd);
+        close(*conn->fd);
+        *conn->fd = -1; 
+      }
+  }
+
+  if (conn->other->duplex == OP_NOOP) { 
+        if (*conn->other->fd != -1) {
+            selector_unregister_fd(s, *conn->other->fd);
+            close(*conn->other->fd);
+            *conn->other->fd = -1;
+        }
+  }
+
   if (conn->duplex == OP_NOOP && conn->other->duplex == OP_NOOP) {
     return DONE;
   }
   return COPY;
 }
 
-static unsigned handle_write_error(struct copy_st* conn) {
+static unsigned handle_write_error(struct copy_st* conn, fd_selector s) {
   shutdown(*conn->fd, SHUT_WR);
   conn->duplex &= ~OP_WRITE;
 
   if (*conn->other->fd != -1) {
     shutdown(*conn->other->fd, SHUT_RD);
     conn->other->duplex &= ~OP_READ;
+  }
+
+  if (conn->duplex == OP_NOOP) {
+      if (*conn->fd != -1) {
+        selector_unregister_fd(s, *conn->fd);
+        close(*conn->fd);
+        *conn->fd = -1;
+      }
+  }
+
+  if (conn->other->duplex == OP_NOOP) {
+     if (*conn->other->fd != -1) {
+        selector_unregister_fd(s, *conn->other->fd);
+        close(*conn->other->fd);
+        *conn->other->fd = -1;
+     }
   }
 
   if (conn->duplex == OP_NOOP && conn->other->duplex == OP_NOOP) {
@@ -99,13 +131,27 @@ unsigned copy_read(struct selector_key* key) {
   ssize_t bytes_read = recv(key->fd, write_ptr, capacity, 0);
 
   if (bytes_read <= 0) {
-    return handle_read_eof(conn);
+    return handle_read_eof(conn, key->s);
   } else {
     buffer_write_adv(conn->rb, bytes_read);
     
     struct socks5* data = ATTACHMENT(key);
     if (key->fd == data->client_fd) {
         metrics_add_bytes_received(bytes_read);
+    }
+    
+    if (conn->other->fd != NULL && *conn->other->fd != -1 && (conn->other->duplex & OP_WRITE)) {
+        size_t pending_bytes;
+        uint8_t* read_ptr = buffer_read_ptr(conn->other->wb, &pending_bytes);
+        if (pending_bytes > 0) {
+             ssize_t bytes_sent = send(*conn->other->fd, read_ptr, pending_bytes, MSG_NOSIGNAL | MSG_DONTWAIT);
+             if (bytes_sent > 0) {
+                 buffer_read_adv(conn->other->wb, bytes_sent);
+                 if (*conn->other->fd == data->client_fd) {
+                      metrics_add_bytes_sent(bytes_sent);
+                 }
+             }
+        }
     }
   }
 
@@ -124,7 +170,7 @@ unsigned copy_write(struct selector_key* key) {
   ssize_t bytes_sent = send(key->fd, read_ptr, pending_bytes, MSG_NOSIGNAL);
 
   if (bytes_sent <= 0) {
-    return handle_write_error(conn);
+    return handle_write_error(conn, key->s);
   } else {
     buffer_read_adv(conn->wb, bytes_sent);
   
